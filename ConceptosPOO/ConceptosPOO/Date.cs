43d62fd981a8cdf9bae055922c79d0642fd2298e
AdrianGaray <adriangaray396@gmail.com>
2022-03-26 18:31:59 -0300

    Finish day class

---
+++ ConceptosPOO/ConceptosPOO/Date.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month,day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException($"Invalid day: {day}");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            // ==== Manera FEA ====
+            //if (year % 4 == 0) // es multiplo de 4
+            //{
+            //    if (year % 100 == 0) // es multiplo de 100
+            //    {
+            //        if (year % 400 == 0)
+            //        {
+            //            // es viciesto
+            //            return true; ;
+            //        }
+            //        else
+            //        {
+            //            // no es viciesto
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        // es viciesto
+            //        return true;
+            //    }
+            //}
+            //else
+            //{
+            //    // no es viciesto
+            //    return false;
+            //}
+
+            // ==== Manera ELEGANTE ====
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            throw new MonthException($"Invalid month:{month}" );
+        }
+
+        public override string ToString()
+        {
+            // return base.ToString();// ejecuta el metodo de la clase padre
+            // ==== Forma Fea: ====
+            // return _year + "/" + _month + "/" + _day; 
+            // Imprime: 2022/2/11
+
+            // ==== Forma Linda: ====
+            // Iterpolacion
+            // return $"{_year}/{_month}/{_day}"; 
+            // Imprime: 2022/2/11
+
+            // Formateo Iterpolacion 
+            // Se rellena con ceros: el mes y el dia.
+            return $"{_year}/{_month:00}/{_day:00}";
+            // Imprime: 2022/02/11
+        }
+    }
+}
---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+// Date date1 = new Date(2022, 2, 11);
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29)); // Biciesto Correcto
+    Console.WriteLine(new Date(2022, 2, 29)); // Biciesto Incorrecto
+    Console.WriteLine(new Date(2022, 12, 7)); // 2022 / 12 / 07
+    Console.WriteLine(new Date(1974, 9, 23)); // 1974 / 09 / 23
+    //Console.WriteLine(new Date(1985, 18, 45)); // Invalid month
+    // Console.WriteLine(new Date(1985, 11, 45));   // Invalid day
+    Console.WriteLine(new Date(1974, 11, 23)); // 1974 / 09 / 23
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
